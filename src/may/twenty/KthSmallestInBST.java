0110a632ba7725d9503fc0a402e58bd1e0fc4415
Ashish Deshpande <ashishdeshpande123@gmail.com>
2020-05-20 23:13:05 +0530

    20th May 2020 solution

---
+++ src/may/twenty/KthSmallestInBST.java
@@ -0,0 +1,31 @@
+package may.twenty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TreeNode {
+	int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class KthSmallestInBST {
+    TreeNode minimumElement(TreeNode root, int val) {
+        
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        while(true) {
+            
+        }
+    }
+}

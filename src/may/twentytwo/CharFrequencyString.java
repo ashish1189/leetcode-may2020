91f235e3e650528b3ec434cfad6a2dfbddf22970
Ashish Deshpande <ashishdeshpande123@gmail.com>
2020-05-23 00:42:24 +0530

    22nd May 2020 solution

--- src/may/twentytwo/CharFrequencyString.java
+++ src/may/twentytwo/CharFrequencyString.java
@@ -1,10 +1,44 @@
 package may.twentytwo;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class CharFrequencyString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public String frequencySort(String s) {
+		Map<Character, Integer> sorted = new HashMap<>();
+		String result = "";
 
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			sorted.put(c, sorted.containsKey(c) ? sorted.get(c) + 1 : 1);
+		}
+		
+		sorted = sorted
+					.entrySet()
+					.stream()
+					.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+					.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+							LinkedHashMap:: new));
+
+		for(Map.Entry<Character, Integer> entry : sorted.entrySet()) {
+			char c = entry.getKey();
+			String repeat = new String(new char[sorted.get(c)]).replaceAll("\0", String.valueOf(c));
+			result += repeat;
+		}
+
+		return result;
+    }
+
+	public static void main(String[] args) {
+		CharFrequencyString cfs = new CharFrequencyString();
+		
+		System.out.println(cfs.frequencySort("Aabb"));
+		System.out.println(cfs.frequencySort("tree"));
+		System.out.println(cfs.frequencySort("cccaaa"));
 	}
 
 }
